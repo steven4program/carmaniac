099d63c6eab803ad25ceaf9b1a5abff2194c5bf0
steven4program <stevenchang421@gmail.com>
2023-06-11 22:57:45 +0800

    feature: finish coupon page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/admin/Dashboard';
-import AdminProducts from './pages/admin/AdminProducts';
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/admin/Dashboard'
+import AdminProducts from './pages/admin/AdminProducts'
+import AdminCoupons from './pages/admin/AdminCoupons'
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/login" element={<Login />}></Route>
-				<Route path="/admin" element={<Dashboard />}>
-					<Route path="products" element={<AdminProducts />}></Route>
-				</Route>
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="products" element={<AdminProducts />}></Route>
+          <Route path="coupons" element={<AdminCoupons />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
